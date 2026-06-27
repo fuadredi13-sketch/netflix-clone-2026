c404ae8ff318f55134770e32c2c2599ff8afcdb4
fuadredi13-sketch <fuadredi13@gmail.com>
2026-06-27 20:15:47 +0300

    Banner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from "./Header/Header.jsx"
 import Footer from './Footer/Footer.jsx'
+import Banner from './banner/Banner.jsx'
 
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
+      
     </div>
   )
 }
